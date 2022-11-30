53369c4779b77eed611b7d4a40f87b0692e2f735
KyleTruong <ktruong008@gmail.com>
2022-11-30 11:08:47 -0500

    Initial work on disabled button classes

--- src/lib/button/button.svelte
+++ src/lib/button/button.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
   import type { BUTTON_SIZE } from './button.type';
 
-  /**
-   * Button types
-   */
+  // Button types
   export let primary = false;
   export let text = false;
   export let link = false;
-  /**
-   * Button size
-   */
+  // Button size
   export let size: BUTTON_SIZE = 'medium';
+  // States
+  export let disabled = false;
 
   const commonClasses = 'box-border whitespace-nowrap';
+  const commonDisabledClasses = `hover:cursor-default text-gray-600`;
 
+  /**
+   * Button sizes
+   */
   const smallSizeClasses = `h-6 text-sm py-0 px-2 rounded`;
   const mediumSizeClasses = `h-8 text-sm font-normal py-1 px-4 rounded-md`;
   const largeSizeClasses = `h-10 text-base font-normal py-2 px-4 rounded-lg`;
 
+  /**
+   * Button types
+   */
   const primaryTypeClasses =
     'bg-blue-600 text-blue-50 hover:bg-blue-500 active:bg-blue-700 transition-all shadow-sm';
   const defaultTypeClasses = `bg-white border-gray-300 border text-gray-900 hover:border-blue-500 hover:text-blue-500 active:border-blue-700 active:text-blue-700 transition-all shadow-sm`;
@@ -26,6 +31,16 @@
   const linkTypeClasses =
     'bg-transparent text-blue-600 hover:underline hover:text-blue-400 active:text-blue-700 transition-all';
 
+  /**
+   * Disabled classes
+   */
+  const primaryTypeDisabledClasses = `${commonDisabledClasses} bg-blue-200`;
+  const defaultTypeDisabledClasses = `${commonDisabledClasses} border-gray-300 text-gray-600`;
+  const textTypeDisabledClasses = `${commonDisabledClasses}`;
+
+  /**
+   * Class wrangling
+   */
   const buttonTypeClasses = primary
     ? primaryTypeClasses
     : text
@@ -41,9 +56,17 @@
       ? largeSizeClasses
       : mediumSizeClasses;
 
+  const disabledClasses = disabled
+    ? primary
+      ? primaryTypeDisabledClasses
+      : text
+      ? textTypeDisabledClasses
+      : defaultTypeDisabledClasses
+    : '';
+
   const buttonClasses = `${commonClasses} ${buttonTypeClasses} ${buttonSizeClasses}`;
 </script>
 
-<button class={buttonClasses} type="button">
+<button class={`${buttonClasses}${disabledClasses}`} type="button" {disabled}>
   <slot />
 </button>
